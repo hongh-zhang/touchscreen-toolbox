5e1937e94262850edd8dfd5ffc9fb88df27a7c0f
hongh-zhang <honghzhang.0@gmail.com>
2022-03-21 18:26:25 +1100

    Integrate DLC
    Former-commit-id: 68afd3dfc20d4f1adc5952e6390242f19ec2ca36

--- touchscreen_toolbox/dlc.py
+++ touchscreen_toolbox/dlc.py
@@ -1,13 +1,40 @@
+import os
 import sys
+import shutil
 import deeplabcut as dlc
 from contextlib import contextmanager
 
-PATH_CFG = 'touchscreen_toolbox/DLC/config.yaml'
+def analyze(path_cfg : str, video : str, verbosity : bool = False):
+    print(f"Analyzing {video}")
 
-def analyze(video : str):
-    with nostdout():  # silence dlc
-        dlc.analyze_video(PATH_CFG, video, videotype='mp4', batchsize=32)
-        dlc.filterpredictions(PATH_CFG, video, videotype='mp4', filtertype='median')
+    if verbosity:
+        dlc.analyze_videos(path_cfg, video, videotype='mp4', batchsize=32)
+        dlc.filterpredictions(path_cfg, video, videotype='mp4', filtertype='median')
+    else:
+        # silence tensorflow
+        os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2' 
+
+        # silence dlc
+        with nostdout():
+            dlc.analyze_videos(path_cfg, video, videotype='mp4', batchsize=32)
+            dlc.filterpredictions(path_cfg, video, videotype='mp4', filtertype='median')
+    
+
+def cleanup(folder_path, folder2move, name):
+
+    _, _, files = list(os.walk(folder_path))[0]
+
+    for f in files:
+        # raw prediction files
+        if f.endswith('.h5') or f.endswith('.pickle'):
+            shutil.move(os.path.join(folder_path, f), 
+                  os.path.join(folder2move, f))
+
+        # coordinates csv
+        elif f.endswith('.csv') and f.startswith(os.path.join(folder_path, name[:-4])):
+            os.rename(os.path.join(folder_path, f), os.path.join(folder_path, name[:-4]+'.csv'))
+            print('renamed csv')
+    print("Reorganized files")
 
 
 # functions to suppress output
@@ -16,7 +43,8 @@ def analyze(video : str):
 # https://stackoverflow.com/questions/2828953/silence-the-stdout-of-a-function-in-python-without-trashing-sys-stdout-and-resto
 
 class DummyFile(object):
-    def write(self, x): pass
+    def write(self, *args, **kwargs): pass
+    def flush(self, *args, **kwargs): pass
 
 @contextmanager
 def nostdout():
