c73630379586c0bea29ba2f49f7c10a28a655b62
hongh-zhang <honghzhang.0@gmail.com>
2022-03-22 16:32:10 +1100

    Complete postprocess

--- touchscreen_toolbox/__init__.py
+++ touchscreen_toolbox/__init__.py
@@ -1,2 +1,4 @@
+from touchscreen_toolbox import utils
 from touchscreen_toolbox import preprocess
+from touchscreen_toolbox import postprocess
 from touchscreen_toolbox.dlc import (analyze, cleanup)
