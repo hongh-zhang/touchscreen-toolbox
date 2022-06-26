7f67f533f0cb1205a2acdb96ef82762f82718d62
hongh-zhang <honghzhang.0@gmail.com>
2022-06-26 12:39:55 +1000

    Update feature.py

--- touchscreen_toolbox/postprocess/feature.py
+++ touchscreen_toolbox/postprocess/feature.py
@@ -7,60 +7,24 @@ from touchscreen_toolbox import utils
 from touchscreen_toolbox import config as cfg
 
 
-def distance(data: pd.DataFrame, pt1: str, pt2: str):
-    """Distance between a pair of keypoints"""
-    dx = data[pt1 + "_x"] - data[pt2 + "_x"]
-    dy = data[pt1 + "_y"] - data[pt2 + "_y"]
-    return np.sqrt(dx ** 2 + dy ** 2)
+
+def engineering(vid_info: dict) -> None:
+    vid_info['data'] = engineering2(vid_info['data'], vid_info['fps'])
 
 
-def velocity1(data: pd.DataFrame, col: str):
-    """1D Velocity of scalar values (from distance)"""
-    return np.diff(data[col], prepend=data[col].iloc[0])
-
-
-def velocity2(data: pd.DataFrame, col: str):
-    """2D Velocity of vector values (from coordinates)"""
-    dx = np.diff(data[col + "_x"], prepend=data[col + "_x"].iloc[0])
-    dy = np.diff(data[col + "_x"], prepend=data[col + "_x"].iloc[0])
-    return np.sqrt(dx ** 2 + dy ** 2)
-
-
-def get_angle(data: pd.DataFrame, pt1: str, pt2: str):
-    """Orientation, defined as the angle between pt1, pt2, horizontal axis"""
-    # calculate angle
-    dx = data[pt1 + "_x"] - data[pt2 + "_x"]
-    dy = data[pt1 + "_y"] - data[pt2 + "_y"]
-    angle = np.arctan2(dy, dx)
-
-    # cast to [0, 2 pi] range
-    angle += (angle < 0).astype(int) * 2 * pi + angle
-
-    return angle
-
-
-def get_angle_v(angles):
-    """Continuous angular velocity"""
-    angles2 = (angles<180).astype(int) * 360 + angles  # ~[180, 360] for continuity
-    v_angles =  utils.absmin(np.diff(angles, prepend=angles.iloc[0]), 
-                             np.diff(angles2, prepend=angles2.iloc[0]))
-    return v_angles
-
-
-
-def engineering(data: pd.DataFrame, fps: int):
-    """Hardecoded feature engineering"""
+def engineering2(data: pd.DataFrame, fps: int) -> pd.DataFrame:
+    """Feature engineering (*hardcoded)"""
     
     data = data.copy()
     
     data['time'] = data['frame'] / fps
     
     # orientation
-    h_angle = get_angle(data, 'snout', 'spine1')
+    neck = (select_bodypart(data, 'spine1') + select_bodypart(data, 'lEar') + select_bodypart(data, 'rEar')) / 3
+    h_angle = get_angle(select_bodypart(data, 'snout'), neck)
     data['head_angle'] = h_angle
     data['v-head_angle'] = get_angle_v(h_angle)
-
-    b_angle = get_angle(data, 'snout', 'tail1')
+    b_angle = get_angle(neck, select_bodypart(data, 'tail1'))
     data['body_angle'] = b_angle
     data['v-body_angle'] = get_angle_v(b_angle)
 
@@ -86,3 +50,51 @@ def engineering(data: pd.DataFrame, fps: int):
     for col in v_cols:
         data["a-" + col[2:]] = velocity1(data, col)
     return data.round(decimals=cfg.DECIMALS)
+
+
+
+# helpers
+# -------
+def distance(data: pd.DataFrame, pt1: str, pt2: str):
+    """Distance between a pair of keypoints"""
+    dx = data[pt1 + "_x"] - data[pt2 + "_x"]
+    dy = data[pt1 + "_y"] - data[pt2 + "_y"]
+    return np.sqrt(dx ** 2 + dy ** 2)
+
+
+def velocity1(data: pd.DataFrame, col: str):
+    """1D Velocity of scalar values (from distance)"""
+    return np.diff(data[col], prepend=data[col].iloc[0])
+
+
+def velocity2(data: pd.DataFrame, col: str):
+    """2D Velocity of vector values (from coordinates)"""
+    dx = np.diff(data[col + "_x"], prepend=data[col + "_x"].iloc[0])
+    dy = np.diff(data[col + "_x"], prepend=data[col + "_x"].iloc[0])
+    return np.sqrt(dx ** 2 + dy ** 2)
+
+
+def select_bodypart(data, bodypart):
+    return data[[bodypart+'_x', bodypart+'_y']].values
+
+
+def get_angle(pts1: np.ndarray, pts2: np.ndarray):
+    """Orientation, defined as the angle between pt1, pt2, horizontal axis"""
+    # calculate angle
+    dx = pts1[:,0] - pts2[:,0]
+    dy = pts1[:,1] - pts2[:,1]
+    angle = np.arctan2(dy, dx)
+
+    # [-180, 180] -> [0, 360]
+    angle += (angle < 0).astype(int) * 2 * pi + angle
+    angle *= 180 / pi
+
+    return angle
+
+
+def get_angle_v(angles: pd.DataFrame):
+    """Continuous angular velocity"""
+    angles2 = (angles<180).astype(int) * 180 + angles  # ~[180, 540] for continuity
+    v_angles =  utils.absmin(np.diff(angles, prepend=angles.iloc[0]), 
+                             np.diff(angles2, prepend=angles2.iloc[0]))
+    return v_angles
