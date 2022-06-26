ee09c58babef0e3840a8a7d9f336a00071a4573b
hongh-zhang <honghzhang.0@gmail.com>
2022-06-26 13:14:07 +1000

    Separate exception logger

--- touchscreen_toolbox/__init__.py
+++ touchscreen_toolbox/__init__.py
@@ -10,9 +10,17 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 file_handler = logging.FileHandler(filename="log.log")
+erro_handler = logging.FileHandler(filename="err.log")
+erro_handler.setLevel(40)
 stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setLevel(20)
+
 formatter = logging.Formatter("%(asctime)s : %(levelname)s : %(name)s : %(message)s")
 file_handler.setFormatter(formatter)
+erro_handler.setFormatter(formatter)
 
 logger.addHandler(file_handler)
+logger.addHandler(erro_handler)
 logger.addHandler(stdout_handler)
+
+logger.debug("Initialized")
